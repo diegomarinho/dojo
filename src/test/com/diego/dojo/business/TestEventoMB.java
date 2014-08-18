01811dfb459cb24e0bb90fa2c21b2d0c4bf1a988
diegomarinho <diegomarinhoalmeida@gmail.com>
2014-08-18 11:52:05 -0300

    Commit ajustes

--- src/test/com/diego/dojo/business/TestEventoMB.java
+++ src/test/com/diego/dojo/business/TestEventoMB.java
@@ -20,12 +20,22 @@ import com.diego.dojo.entity.Pontuacao;
  * @since 18/08/2014
  */
 public class TestEventoMB {
+	
+	@Test
+	public void testNovaPartida() {
+		EventoMB servico = new EventoMB();
+		Date data = new Date();
+		Partida partida = servico.criarPartida(data, "New match 11348965 has started");
+
+		Assert.assertEquals(data, partida.getInicio());
+		Assert.assertEquals("11348965", partida.getNome());
+	}
 
 	@Test
-	public void testRetornarPontuacaoJogadorNovo() {
+	public void testPontuacaoNovoJogador() {
 
 		EventoMB servico = new EventoMB();
-		Jogador jogador = new Jogador("David");
+		Jogador jogador = new Jogador("Diego");
 		Pontuacao pontuacao = servico.retornarPontuacao(new HashSet<Pontuacao>(), jogador);
 
 		Assert.assertEquals(0, pontuacao.getMatou());
@@ -35,9 +45,9 @@ public class TestEventoMB {
 	}
 
 	@Test
-	public void testRetornarPontuacaoJogadorExistente() {
+	public void testPontuacaoJogadorExistente() {
 		EventoMB servico = new EventoMB();
-		Jogador jogador = new Jogador("David");
+		Jogador jogador = new Jogador("Diego");
 		Set<Pontuacao> pontuacoes = new HashSet<Pontuacao>();
 		Pontuacao pontuacao = new Pontuacao(jogador);
 		pontuacao.adicionarAssassinato();
@@ -53,93 +63,14 @@ public class TestEventoMB {
 		Assert.assertEquals(jogador, pontuacaoRetornada.getJogador());
 		Assert.assertEquals(1, pontuacaoRetornada.getArmas().size());
 	}
-
+	
 	@Test
-	public void testAdicionarMorte() {
+	public void testAdicionarCincoMortes() {
 		EventoMB servico = new EventoMB();
-		Jogador matador = new Jogador("matador");
-		Jogador morto = new Jogador("morto");
-		Arma arma = new Arma("arma");
-		Morte morte = new Morte(matador, morto, arma, new Date());
-		Partida partida = new Partida(new Date(), "nomePartida");
-		servico.adicionarMorte(partida, morte);
-
-		Pontuacao pontuacaoMatador = servico.retornarPontuacao(partida.getPontuacoes(), matador);
-		Assert.assertEquals(1, pontuacaoMatador.getMatou());
-		Assert.assertEquals(0, pontuacaoMatador.getMorreu());
-		Assert.assertEquals(matador, pontuacaoMatador.getJogador());
-		Assert.assertEquals(1, pontuacaoMatador.getArmas().get(arma).intValue());
-
-		Pontuacao pontuacaoMorto = servico.retornarPontuacao(partida.getPontuacoes(), morto);
-		Assert.assertEquals(0, pontuacaoMorto.getMatou());
-		Assert.assertEquals(1, pontuacaoMorto.getMorreu());
-		Assert.assertEquals(morto, pontuacaoMorto.getJogador());
-		Assert.assertEquals(0, pontuacaoMorto.getArmas().size());
-	}
-
-	@Test
-	public void testCriarNovaPartida() {
-		EventoMB servico = new EventoMB();
-		Date data = new Date();
-		Partida partida = servico.criarPartida(data, "New match 11348965 has started");
-
-		Assert.assertEquals(data, partida.getInicio());
-		Assert.assertEquals("11348965", partida.getNome());
-	}
-
-	@Test
-	public void testAdicionarCincoMortesUmMinuto() {
-		EventoMB servico = new EventoMB();
-		Jogador matador = new Jogador("matador");
-		Jogador morto = new Jogador("morto");
-		Arma arma = new Arma("arma");
-		Partida partida = new Partida(new Date(), "nomePartida");
-
-		Morte morte = new Morte(matador, morto, arma, new Date());
-		servico.adicionarMorte(partida, morte);
-		Morte morte2 = new Morte(matador, morto, arma, new Date());
-		servico.adicionarMorte(partida, morte2);
-		Morte morte3 = new Morte(matador, morto, arma, new Date());
-		servico.adicionarMorte(partida, morte3);
-		Morte morte4 = new Morte(matador, morto, arma, new Date());
-		servico.adicionarMorte(partida, morte4);
-		Morte morte5 = new Morte(matador, morto, arma, new Date());
-		servico.adicionarMorte(partida, morte5);
-
-		Assert.assertTrue(partida.getJogadorMatouMaisDeCincoVezesUmMinuto().contains(matador));
-	}
-
-	@Test
-	public void testAdicionarCincoMortesMaisDeUmMinuto() {
-		EventoMB servico = new EventoMB();
-		Jogador matador = new Jogador("matador");
-		Jogador morto = new Jogador("morto");
-		Arma arma = new Arma("arma");
-		Partida partida = new Partida(new Date(), "nomePartida");
-
-		Morte morte = new Morte(matador, morto, arma, new Date());
-		servico.adicionarMorte(partida, morte);
-		Morte morte2 = new Morte(matador, morto, arma, new Date());
-		servico.adicionarMorte(partida, morte2);
-		Morte morte3 = new Morte(matador, morto, arma, new Date());
-		servico.adicionarMorte(partida, morte3);
-		Morte morte4 = new Morte(matador, morto, arma, new Date());
-		servico.adicionarMorte(partida, morte4);
-		Calendar c = Calendar.getInstance();
-		c.add(Calendar.MINUTE, 1);
-		Morte morte5 = new Morte(matador, morto, arma, c.getTime());
-		servico.adicionarMorte(partida, morte5);
-
-		Assert.assertFalse(partida.getJogadorMatouMaisDeCincoVezesUmMinuto().contains(matador));
-	}
-
-	@Test
-	public void testAdicionarCincoMortesUmMinutoFim() {
-		EventoMB servico = new EventoMB();
-		Jogador matador = new Jogador("matador");
-		Jogador morto = new Jogador("morto");
-		Arma arma = new Arma("arma");
-		Partida partida = new Partida(new Date(), "nomePartida");
+		Jogador matador = new Jogador("Diego");
+		Jogador morto = new Jogador("Jonas");
+		Arma arma = new Arma("Glock");
+		Partida partida = new Partida(new Date(), "Partida1");
 
 		Morte morte = new Morte(matador, morto, arma, new Date());
 		servico.adicionarMorte(partida, morte);
@@ -163,6 +94,75 @@ public class TestEventoMB {
 		servico.adicionarMorte(partida, morte9);
 
 		Assert.assertTrue(partida.getJogadorMatouMaisDeCincoVezesUmMinuto().contains(matador));
+	}
+
+	@Test
+	public void testMorteJogador() {
+		EventoMB servico = new EventoMB();
+		Jogador matador = new Jogador("Diego");
+		Jogador morto = new Jogador("Jonas");
+		Arma arma = new Arma("Glock");
+		Morte morte = new Morte(matador, morto, arma, new Date());
+		Partida partida = new Partida(new Date(), "Partida2");
+		servico.adicionarMorte(partida, morte);
+
+		Pontuacao pontuacaoMatador = servico.retornarPontuacao(partida.getPontuacoes(), matador);
+		Assert.assertEquals(1, pontuacaoMatador.getMatou());
+		Assert.assertEquals(0, pontuacaoMatador.getMorreu());
+		Assert.assertEquals(matador, pontuacaoMatador.getJogador());
+		Assert.assertEquals(1, pontuacaoMatador.getArmas().get(arma).intValue());
+
+		Pontuacao pontuacaoMorto = servico.retornarPontuacao(partida.getPontuacoes(), morto);
+		Assert.assertEquals(0, pontuacaoMorto.getMatou());
+		Assert.assertEquals(1, pontuacaoMorto.getMorreu());
+		Assert.assertEquals(morto, pontuacaoMorto.getJogador());
+		Assert.assertEquals(0, pontuacaoMorto.getArmas().size());
+	}
+
+	@Test
+	public void testAdicionarCincoMortesAbaixoUmMinuto() {
+		EventoMB servico = new EventoMB();
+		Jogador matador = new Jogador("Diego");
+		Jogador morto = new Jogador("Jonas");
+		Arma arma = new Arma("Glock");
+		Partida partida = new Partida(new Date(), "Partida3");
+
+		Morte morte = new Morte(matador, morto, arma, new Date());
+		servico.adicionarMorte(partida, morte);
+		Morte morte2 = new Morte(matador, morto, arma, new Date());
+		servico.adicionarMorte(partida, morte2);
+		Morte morte3 = new Morte(matador, morto, arma, new Date());
+		servico.adicionarMorte(partida, morte3);
+		Morte morte4 = new Morte(matador, morto, arma, new Date());
+		servico.adicionarMorte(partida, morte4);
+		Morte morte5 = new Morte(matador, morto, arma, new Date());
+		servico.adicionarMorte(partida, morte5);
+
+		Assert.assertTrue(partida.getJogadorMatouMaisDeCincoVezesUmMinuto().contains(matador));
+	}
+
+	@Test
+	public void testAdicionarCincoMortesAcimaUmMinuto() {
+		EventoMB servico = new EventoMB();
+		Jogador matador = new Jogador("Diego");
+		Jogador morto = new Jogador("Jonas");
+		Arma arma = new Arma("Glock");
+		Partida partida = new Partida(new Date(), "Partida4");
+
+		Morte morte = new Morte(matador, morto, arma, new Date());
+		servico.adicionarMorte(partida, morte);
+		Morte morte2 = new Morte(matador, morto, arma, new Date());
+		servico.adicionarMorte(partida, morte2);
+		Morte morte3 = new Morte(matador, morto, arma, new Date());
+		servico.adicionarMorte(partida, morte3);
+		Morte morte4 = new Morte(matador, morto, arma, new Date());
+		servico.adicionarMorte(partida, morte4);
+		Calendar c = Calendar.getInstance();
+		c.add(Calendar.MINUTE, 1);
+		Morte morte5 = new Morte(matador, morto, arma, c.getTime());
+		servico.adicionarMorte(partida, morte5);
+
+		Assert.assertFalse(partida.getJogadorMatouMaisDeCincoVezesUmMinuto().contains(matador));
 	}
 
 }
--- src/test/com/diego/dojo/entity/TestPontuacao.java
+++ src/test/com/diego/dojo/entity/TestPontuacao.java
@@ -14,44 +14,38 @@ import com.diego.dojo.entity.Pontuacao;
 public class TestPontuacao {
 
 	@Test
-	public void testNovaArma() {
-		final Pontuacao pontuacao = new Pontuacao(new Jogador("David"));
-
-		final Arma arma = new Arma("Arma");
+	public void testArmaCombinada() {
+		Pontuacao pontuacao = new Pontuacao(new Jogador("Diego"));
+		Arma arma = new Arma("Glock");
 		pontuacao.adicionarArma(arma);
-
+		pontuacao.adicionarArma(arma);
+		Assert.assertEquals(2, pontuacao.getArmas().get(arma).intValue());
+	}
+	
+	@Test
+	public void testArmaSucesso() {
+		Pontuacao pontuacao = new Pontuacao(new Jogador("Diego"));
+		Arma arma = new Arma("Glock");
+		pontuacao.adicionarArma(arma);
 		Assert.assertEquals(1, pontuacao.getArmas().get(arma).intValue());
 	}
 
 	@Test
-	public void testArmaExistente() {
-		final Pontuacao pontuacao = new Pontuacao(new Jogador("David"));
-
-		final Arma arma = new Arma("Arma");
-		pontuacao.adicionarArma(arma);
-		pontuacao.adicionarArma(arma);
-
-		Assert.assertEquals(2, pontuacao.getArmas().get(arma).intValue());
-	}
-
-	@Test
-	public void testMultiplasArmas() {
-		final Pontuacao pontuacao = new Pontuacao(new Jogador("David"));
-
-		final Arma arma = new Arma("Arma");
-		final Arma arma2 = new Arma("Arma2");
-		final Arma arma3 = new Arma("Arma3");
-		pontuacao.adicionarArma(arma);
-		pontuacao.adicionarArma(arma);
+	public void testArmaListagem() {
+		Pontuacao pontuacao = new Pontuacao(new Jogador("Diego"));
+		Arma arma1 = new Arma("M16");
+		Arma arma2 = new Arma("Glock");
+		Arma arma3 = new Arma("AWP");
+		pontuacao.adicionarArma(arma1);
+		pontuacao.adicionarArma(arma1);
 		pontuacao.adicionarArma(arma2);
 		pontuacao.adicionarArma(arma3);
-		pontuacao.adicionarArma(arma);
-		pontuacao.adicionarArma(arma);
+		pontuacao.adicionarArma(arma1);
+		pontuacao.adicionarArma(arma1);
 		pontuacao.adicionarArma(arma3);
 		pontuacao.adicionarArma(arma2);
 		pontuacao.adicionarArma(arma3);
-
-		Assert.assertEquals(4, pontuacao.getArmas().get(arma).intValue());
+		Assert.assertEquals(4, pontuacao.getArmas().get(arma1).intValue());
 		Assert.assertEquals(2, pontuacao.getArmas().get(arma2).intValue());
 		Assert.assertEquals(3, pontuacao.getArmas().get(arma3).intValue());
 	}
